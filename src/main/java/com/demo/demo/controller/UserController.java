188aebea94081e1dc676b2a1405746d311971d79
Ramesh m <rameshmaneddi6@gmail.com>
2025-08-24 04:34:00 -0700

    resume

--- src/main/java/com/demo/demo/controller/UserController.java
+++ src/main/java/com/demo/demo/controller/UserController.java
@@ -1,14 +1,31 @@
-//package com.demo.demo.controller;
-//
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class UserController {
-//	
-//	@GetMapping(value = "/test")
-//	public String message() {
-//		return "deoloy success";
-//	}
-//
-//}
+package com.demo.demo.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	
+	@GetMapping("/resume")
+    public ResponseEntity<Resource> downloadResume() throws IOException {
+        File file = new File("src/main/resources/Ramesh_Maneddi_Java_Developer.pdf");
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=resume.pdf")
+                .contentType(MediaType.APPLICATION_PDF)
+                .contentLength(file.length())
+                .body(resource);
+    }
+
+}
+
